611323290e13c25d5a1615d2e7217aea6de70174
Romil Narola <romil@divynesystems.com>
2026-08-05 14:40:44 +0530

    chore: update android namespace and applicationId to com.example.ledger_flow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.ledgerflow.app"
+    namespace = "com.example.ledger_flow"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.ledgerflow.app"
+        applicationId = "com.example.ledger_flow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
